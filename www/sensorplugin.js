298b9f0372ab78b50ec37dc56936c4179429abba
tonimichel-hdm <tm063@hdm-stuttgart.de>
2014-10-01 21:50:24 +0200

    test

--- www/sensorplugin.js
+++ www/sensorplugin.js
@@ -3,7 +3,7 @@ var sensorplugin = {
         cordova.exec(
             successCallback, // success callback function
             errorCallback, // error callback function
-            'Calendar', // mapped to our native Java class called "CalendarPlugin"
+            'SensorPlugin', // mapped to our native Java class called "CalendarPlugin"
             'addCalendarEntry', // with this action name
             [{                  // and this array of custom arguments to create our entry
                 "title": title,
@@ -19,7 +19,7 @@ var sensorplugin = {
         cordova.exec( 
             successCallback,
             errorCallback,
-            'Calendar',
+            'SensorPlugin',
             'putEcho',
             [ msg ]
         );
